9c9c8f079d17f8b2386de65fbf57215c53cb94d3
shamimice03 <shamimice03@gmail.com>
2025-01-05 03:05:47 +0900

    sg updated

--- sg.tf
+++ sg.tf
@@ -31,9 +31,9 @@ resource "aws_security_group" "private_instance" {
   vpc_id      = module.vpc.vpc_id
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow HTTPS to internet via NAT Gateway"
   }
